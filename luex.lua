893020e94df8a63867c951a02ee764d909de017d
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-08-25 19:24:07 +0700

    Update luex.lua

--- luex.lua
+++ luex.lua
@@ -1,4 +1,4 @@
 local loading = loadstring
-local game = game:HttpGet
+local get = game:HttpGet
 local url = 'https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/luexfile.lua'
 loading(game(url))()
