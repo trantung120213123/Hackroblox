62e26f3ad402defb8f6b73d4c63e5e47c5ec005d
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-08-25 19:17:10 +0700

    Update luexfile.lua

--- luexfile.lua
+++ luexfile.lua
@@ -1,5 +1,6 @@
 -- luôn load Luexhubreal.lua
-loadstring(game:HttpGet("https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/Luexhubreal.lua"))()
+local url = 'https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/Luexhubreal.lua'
+loadstring(game:HttpGet(url))()
 
 -- chỉ load antiafk.lua nếu bật
 if getgenv().antiafk == "true" then
