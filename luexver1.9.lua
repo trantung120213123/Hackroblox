b868684ebd723448465769bd633f0043ccf296ad
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-08-25 20:47:53 +0700

    Update luexver1.9.lua

--- luexver1.9.lua
+++ luexver1.9.lua
@@ -2,10 +2,20 @@
 local url = 'https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/Luexhubreal.lua'
 loadstring(game:HttpGet(url))()
 
--- chỉ load antiafk.lua nếu bật
-if getgenv().antiafk == "true" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/antiafk.lua"))()
-    print("[✅] Anti-AFK đã bật")
-else
-    print("[ℹ️] Luexhubreal đã load, Anti-AFK đang tắt")
-end
+--// Anti AFK Script
+local vu = game:GetService("VirtualUser")
+local plr = game:GetService("Players").LocalPlayer
+
+plr.Idled:Connect(function()
+    if getgenv().antiafk == true then
+        vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+        task.wait(1)
+        vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+        print("[Anti AFK] Đã click giả để tránh kick AFK")
+        warn("[Anti AFK] Đang hoạt động...")
+    else
+        print("[Anti AFK] Script đã bị tắt")
+    end
+end)
+
+print("[Anti AFK] Script đã load xong. Trạng thái: "..tostring(getgenv().antiafk))
